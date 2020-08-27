71323b808a888dce18cd73014fce7f401761681a
kiki <32135666+kikihiter@users.noreply.github.com>
2020-08-27 19:32:11 +0800

    Add files via upload

---
+++ Medium/No138o2.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    """
+    :type head: Node
+    :rtype: Node
+    """
+    def __init__(self):
+        # Dictionary which holds old nodes as keys and new nodes as its values.
+        self.visitedHash = {}
+
+    def copyRandomList(self, head):
+
+        if head == None:
+            return None
+
+        # If we have already processed the current node, then we simply return the cloned version of it.
+        if head in self.visitedHash:
+            return self.visitedHash[head]
+
+        # create a new node
+        # with the value same as old node.
+        node = Node(head.val, None, None)
+
+        # Save this value in the hash map. This is needed since there might be
+        # loops during traversal due to randomness of random pointers and this would help us avoid them.
+        self.visitedHash[head] = node
+
+        # Recursively copy the remaining linked list starting once from the next pointer and then from the random pointer.
+        # Thus we have two independent recursive calls.
+        # Finally we update the next and random pointers for the new node created.
+        node.next = self.copyRandomList(head.next)
+        node.random = self.copyRandomList(head.random)
+
+        return node
+
+作者：LeetCode
+链接：https://leetcode-cn.com/problems/copy-list-with-random-pointer/solution/fu-zhi-dai-sui-ji-zhi-zhen-de-lian-biao-by-leetcod/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+"""
+回溯，在函数中添加了一个self.visitedHash = {}
+"""
