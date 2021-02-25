3844ef13987c72e7910a5057d3ad903d7cde5057
kiki <32135666+kikihiter@users.noreply.github.com>
2021-02-25 20:18:02 +0800

    Add files via upload

---
+++ Medium/No198s.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        size = len(nums)
+        if size == 1:
+            return nums[0]
+        
+        first, second = nums[0], max(nums[0], nums[1])
+        for i in range(2, size):
+            first, second = second, max(first + nums[i], second)
+        
+        return second
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/house-robber/solution/da-jia-jie-she-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
