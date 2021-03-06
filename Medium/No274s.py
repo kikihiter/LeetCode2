281a0260f221e60e0d93b74614fe00639f15be9c
kiki <32135666+kikihiter@users.noreply.github.com>
2021-03-06 20:50:44 +0800

    Add files via upload

---
+++ Medium/No274s.py
@@ -0,0 +1,23 @@
+class Solution:
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        n = len(citations)
+        left, right = 0, n - 1
+        while left <= right:
+            pivot = left + (right - left) // 2
+            if citations[pivot] == n - pivot:
+                return n - pivot
+            elif citations[pivot] < n - pivot:
+                left = pivot + 1
+            else:
+                right = pivot - 1
+        
+        return n - left
+
+作者：LeetCode
+链接：https://leetcode-cn.com/problems/h-index-ii/solution/hzhi-shu-ii-by-leetcode/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
