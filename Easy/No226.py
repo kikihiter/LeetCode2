13b654e868e43da0d91b9b44f6b05f3764e241a1
kiki <32135666+kikihiter@users.noreply.github.com>
2021-03-22 22:47:36 +0800

    Add files via upload

---
+++ Easy/No226.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        """
+        210322
+        """
+        if not root:
+            return
+        if not root.left and not root.right:
+            return root
+        root.left, root.right = root.right, root.left
+        if root.left:
+            self.invertTree(root.left)
+        if root.right:
+            self.invertTree(root.right)
+        return root
