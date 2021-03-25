28a9bcfd8105f20b20b137cf6e117259c321a95d
kiki <32135666+kikihiter@users.noreply.github.com>
2021-03-25 18:04:52 +0800

    Add files via upload

---
+++ Everyday/No082s.py
@@ -0,0 +1,22 @@
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+        
+        dummy = ListNode(0, head)
+
+        cur = dummy
+        while cur.next and cur.next.next:
+            if cur.next.val == cur.next.next.val:
+                x = cur.next.val
+                while cur.next and cur.next.val == x:
+                    cur.next = cur.next.next
+            else:
+                cur = cur.next
+
+        return dummy.next
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list-ii/solution/shan-chu-pai-xu-lian-biao-zhong-de-zhong-oayn/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
